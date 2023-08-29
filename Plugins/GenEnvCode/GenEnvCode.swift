ddf547873f54394dcedf1713e49f7c11fa4f7252
zunda <zunda.dev@gmail.com>
2023-08-30 00:18:57 +0900

    remove ArgumentError

--- Plugins/GenEnvCode/GenEnvCode.swift
+++ Plugins/GenEnvCode/GenEnvCode.swift
@@ -11,25 +11,9 @@ struct GenEnvCode: CommandPlugin {
     let tool = try context.tool(named: "GenEnvCodeExe")
     let toolPath = URL(fileURLWithPath: tool.path.string)
     
-    guard let namespace = arguments[safe: 0] else {
-      throw ArgumentError.noNameSpaceArgument
-    }
-
-    guard let envFilePath = arguments[safe: 1] else {
-      throw ArgumentError.noEnvironmentFilePathArgument
-    }
-    
-    guard let outputFilePath = arguments[safe: 2] else {
-      throw ArgumentError.noOutputFilePathArgument
-    }
-    
     let process = try Process.run(
       toolPath,
-      arguments: [
-        namespace,
-        envFilePath,
-        outputFilePath,
-      ]
+      arguments: arguments
     )
     process.waitUntilExit()
   }
@@ -38,19 +22,5 @@ struct GenEnvCode: CommandPlugin {
 extension Array {
   subscript(safe index: Index) -> Element? {
     return indices.contains(index) ? self[index] : nil
-  }
-}
-
-enum ArgumentError: Error, LocalizedError {
-  case noNameSpaceArgument
-  case noEnvironmentFilePathArgument
-  case noOutputFilePathArgument
-  
-  var helpAnchor: String {
-    switch self {
-    case .noNameSpaceArgument: "Generating Namespace(ex: SecureEnv)."
-    case .noEnvironmentFilePathArgument: "Env file path."
-    case .noOutputFilePathArgument: "Output file path."
-    }
   }
 }
