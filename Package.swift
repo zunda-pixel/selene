48409e0a6c19460939c233a637fbe2af0255ad79
zunda <zunda.dev@gmail.com>
2023-09-01 11:36:54 +0900

    set platform

--- Package.swift
+++ Package.swift
@@ -6,11 +6,13 @@ import PackageDescription
 let package = Package(
   name: "Selene",
   platforms: [
+    .macOS(.v10_15),
     .iOS(.v13),
-    .macOS(.v10_15)
+    .tvOS(.v13),
+    .watchOS(.v6),
+    .macCatalyst(.v13),
   ],
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
     .plugin(
       name: "GenerateCode",
       targets: ["GenerateCode"]
