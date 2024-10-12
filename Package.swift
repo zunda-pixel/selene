6e6112c5b09394682b083754aeab20c630659137
zunda <zunda.dev@gmail.com>
2024-10-13 00:57:17 +0900

    update library

--- Package.swift
+++ Package.swift
@@ -24,8 +24,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
     .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
-    .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"601.0.0-prerelease"),
-    .package(url: "https://github.com/swiftlang/swift-testing", from: "0.11.0"),
+    .package(url: "https://github.com/swiftlang/swift-syntax", from: "600.0.0"),
   ],
   targets: [
     .plugin(
@@ -54,7 +53,6 @@ let package = Package(
       dependencies: [
         .target(name: "Selene"),
         .product(name: "SwiftParser", package: "swift-syntax"),
-        .product(name: "Testing", package: "swift-testing"),
       ]
     ),
   ]
